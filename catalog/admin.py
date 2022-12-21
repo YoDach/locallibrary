19f3a538379fb2c73ac8964486bbab3cf465bd71
YoBilga <d9281024286@yandex.ru>
2022-12-21 04:00:38 +0300

    Initial commit

---
+++ catalog/admin.py
@@ -0,0 +1,55 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
+
+admin.site.register(Genre)
+admin.site.register(Language)
+
+
+class BookInline(admin.TabularInline):
+    model = Book
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+    view_on_site = False
+
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BookInline]
+
+    search_fields = ['last_name']
+
+
+admin.site.register(Author, AuthorAdmin)
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+    inlines = [BookInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability',{
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
+
+
+
+
+
